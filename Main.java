dfe4d6e23de08ebcdd7086603751637b6a80453f
Nimrath K <109793342+nimrathk@users.noreply.github.com>
2023-03-21 16:30:17 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+//  Phoenix.json
+//      https://maven.ctr-electronics.com/release/com/ctre/phoenix/Phoenix5-frc2023-latest.json
+
+//  REVLib.json
+//      https://software-metadata.revrobotics.com/REVLib-2023.json
+
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
