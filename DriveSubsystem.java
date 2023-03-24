b47b600d0c24170462c19326b9910aa0b25fd601
Nimrath K <109793342+nimrathk@users.noreply.github.com>
2023-03-24 15:18:11 -0400

    Update DriveSubsystem.java

--- DriveSubsystem.java
+++ DriveSubsystem.java
@@ -9,12 +9,16 @@ import frc.robot.Constants.DriveConstants;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 public class DriveSubsystem extends SubsystemBase {
 
+  // Driving motors
   private final WPI_VictorSPX m_leftFrontMotor = new WPI_VictorSPX(DriveConstants.kLeftFrontMotorPort);
   private final WPI_VictorSPX m_rightFrontMotor = new WPI_VictorSPX(DriveConstants.kRightFrontMotorPort);
   private final WPI_VictorSPX m_leftRearMotor = new WPI_VictorSPX(DriveConstants.kLeftBackMotorPort);
@@ -24,15 +28,43 @@ public class DriveSubsystem extends SubsystemBase {
   private final MotorControllerGroup rightMotors = new MotorControllerGroup(m_rightFrontMotor, m_rightRearMotor);
   private DifferentialDrive diffDrive = new DifferentialDrive(leftMotors, rightMotors);
 
-  /** Creates a new ExampleSubsystem. */
+  private Encoder leftEncoder = new Encoder(DriveConstants.kLeftEncoderChannelA, DriveConstants.kLeftEncoderChannelB);
+  private Encoder rightEncoder = new Encoder(DriveConstants.kRightEncoderChannelA, DriveConstants.kRightEncoderChannelB);
+
+  public double getEncoderFeetAverage()
+  {
+    return (getLeftEncoderFeet() + getRightEncoderFeet() / 2);
+  }
+            
+  public double getLeftEncoderFeet() {
+    double leftEncoderFeet = leftEncoder.get() * DriveConstants.kEncoderTick2Feet;
+    return leftEncoderFeet;
+  }
+
+  public double getRightEncoderFeet() {
+    double rightEncoderFeet = -rightEncoder.get() * DriveConstants.kEncoderTick2Feet;
+    return rightEncoderFeet;
+  }
+
+  public void resetEncoders()
+  {
+    leftEncoder.reset();
+    rightEncoder.reset();
+  }
+
+  /** Creates a new DriveSubsystem object (this is the constructor). */
   public DriveSubsystem() {
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Left Encoder Feet", getLeftEncoderFeet());
+    SmartDashboard.putNumber("Right Encoder Feet", getRightEncoderFeet());
+    SmartDashboard.putNumber("AVERAGE Encoder Feet", getEncoderFeetAverage());
   }
-
+  
+  // This lets us alter the motor values for driving
   public void setMotors(double leftSpeed, double rightSpeed)
   {
     diffDrive.arcadeDrive(leftSpeed, rightSpeed);
