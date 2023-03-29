53efee50d2141afa51617c08ff7561bf5a7a1c0e
Nimrath K <109793342+nimrathk@users.noreply.github.com>
2023-03-29 06:42:45 -0400

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -32,4 +32,48 @@ public final class Constants {
     public static final double kAutoDriveForwardSpeed = -0.5;
     public static final double kAutoDriveForwardDistance = 3;
   }
+
+  public static final class ElevatorConstants {
+    public static final int kMotorPort = 9;
+    public static final double kEncoderTick2Meter = 1.0 / 4096.0 * 0.1 * Math.PI;
+
+    public static final double kElevatorPowerOut = 1.0;
+    public static final double kElevatorPowerIn = -0.7;
+    public static final double kElevatorCLRampRate = 0.5;
+
+    public static final double kP = 0.05;
+    public static final double kI = 1e-8;
+    public static final double kD = 0;
+    public static final double kIZone = 0;
+    public static final double kFF = 0;
+  }
+
+  public static final class GripperConstants {
+    public static final int kMotorPort = 8;
+    public static final double kGripperPowerOut = 1.0;
+    public static final double kGripperPowerIn = -1.0;
+    private static final double kGripperCLRampRate = 0.5;
+    public static final double kEncoderTick2Meter = 1.0 / 4096.0 * 0.1 * Math.PI;
+
+    public static final double kP = 6e-5;
+    public static final double kI = 0;
+    public static final double kD = 0;
+    public static final double kIZone = 0;
+    public static final double kFF = 0.00015;
+  }
+
+  public static final class PivotConstants {
+    public static final int kMotorPort = 4;
+    public static final double kEncoderTick2Meter = 1.0 / 4096.0 * 0.1 * Math.PI;
+    
+    public static final double kPivotPowerOut = 1.0;
+    public static final double kPivotPowerIn = -0.7;
+    public static final double kPivotCLRampRate = 0.5;
+
+    public static final double kP = 0.05;
+    public static final double kI = 0;
+    public static final double kD = 0;
+    public static final double kIZone = 0;
+    public static final double kFF = 0.00015;
+  }
 }
