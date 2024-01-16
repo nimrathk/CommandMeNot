6ebd539c8eb33315ef075809e6268367372dd26f
Nimrath K <109793342+nimrathk@users.noreply.github.com>
2024-01-15 19:43:32 -0500

    Update RobotContainer.java

--- RobotContainer.java
+++ RobotContainer.java
@@ -5,13 +5,28 @@
 package frc.robot;
 
 import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.Constants.GripperConstants;
+import frc.robot.Constants.Joystick;
+import frc.robot.Constants.PivotConstants;
 import frc.robot.commands.DriveForwardCmd;
-import frc.robot.commands.Autos;
+import frc.robot.commands.DriveSlewFalseCmd;
+import frc.robot.commands.DriveSlewTrueCmd;
 import frc.robot.commands.ArcadeDriveCmd;
 import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.ElevatorPIDCmd;
+
+import frc.robot.commands.PivotPIDCmd;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.GripperSubsystem;
+import frc.robot.commands.GripperJoyCmd;
+import frc.robot.subsystems.PivotSubsystem;
 
 // import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 // import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -24,16 +39,20 @@ import edu.wpi.first.wpilibj.XboxController;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final DriveSubsystem driveSubsystem = new DriveSubsystem();
-
   private final XboxController m_stickDrive = new XboxController(0);
+  private final XboxController m_stickArm = new XboxController(1);
+
+  // The robot's subsystems are defined here...
+  private final DriveSubsystem driveSubsystem = new DriveSubsystem();
+  private final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
+  private final PivotSubsystem pivotSubsystem = new PivotSubsystem();
+  private final GripperSubsystem gripperSubsystem = new GripperSubsystem();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
-    driveSubsystem.setDefaultCommand(new ArcadeDriveCmd(driveSubsystem, () -> m_stickDrive.getLeftX(), () -> m_stickDrive.getLeftY()));
+    driveSubsystem.setDefaultCommand(new ArcadeDriveCmd(driveSubsystem, () -> m_stickDrive.getLeftY(), () -> m_stickDrive.getLeftX()));
   }
 
   /**
@@ -46,16 +65,54 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
+    // Drive (in)
+    new JoystickButton(m_stickArm, Joystick.kButtonY)
+                .onTrue(new ParallelCommandGroup(new ElevatorPIDCmd(elevatorSubsystem, ElevatorConstants.drivePosition), (new PivotPIDCmd(pivotSubsystem, PivotConstants.drivePosition))));
+
+    // Score (out)
+    new JoystickButton(m_stickArm, Joystick.kButtonB)
+                .onTrue(new ParallelCommandGroup(new SequentialCommandGroup(new WaitCommand(0.25), new ElevatorPIDCmd(elevatorSubsystem, ElevatorConstants.ScorePosition)), (new PivotPIDCmd(pivotSubsystem, PivotConstants.ScorePosition))));
+
+    // Ground (down)
+    new JoystickButton(m_stickArm, Joystick.kButtonA)
+                .onTrue(new ParallelCommandGroup(new ElevatorPIDCmd(elevatorSubsystem, ElevatorConstants.groundPosition), new SequentialCommandGroup(new WaitCommand(0.5), new PivotPIDCmd(pivotSubsystem, PivotConstants.GroundPosition))));
+
+    // Gripper
+    new JoystickButton(m_stickArm, Joystick.leftBtn)
+                .whileTrue(new GripperJoyCmd(gripperSubsystem, GripperConstants.speed));
+    new JoystickButton(m_stickArm, Joystick.rightBtn)
+                .whileTrue(new GripperJoyCmd(gripperSubsystem, -GripperConstants.speed));  
+
+    new JoystickButton(m_stickDrive, Joystick.kButtonB)
+                .whileTrue(new DriveSlewTrueCmd(driveSubsystem));
+    new JoystickButton(m_stickDrive, Joystick.kButtonA)
+                .whileTrue(new DriveSlewFalseCmd(driveSubsystem));
   }
 
   /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return new SequentialCommandGroup(new DriveForwardCmd(driveSubsystem, DriveConstants.kAutoDriveForwardDistance));
-
+    return new SequentialCommandGroup(
+      new ParallelRaceGroup(
+        new ParallelCommandGroup(
+          new SequentialCommandGroup(
+              new WaitCommand(0.25), 
+              new ElevatorPIDCmd(elevatorSubsystem, ElevatorConstants.ScorePosition)), 
+          new PivotPIDCmd(pivotSubsystem, PivotConstants.ScorePosition)), 
+        new WaitCommand(2)),
+      new ParallelRaceGroup(
+        new GripperJoyCmd(gripperSubsystem, -GripperConstants.speed),
+        new WaitCommand(0.5)),
+      new WaitCommand(0.75),
+      new ParallelRaceGroup(
+        new ParallelCommandGroup(
+          new ElevatorPIDCmd(elevatorSubsystem, ElevatorConstants.drivePosition), 
+          (new PivotPIDCmd(pivotSubsystem, PivotConstants.drivePosition))),
+        new WaitCommand(1.5)
+        ),    
+    new DriveForwardCmd(driveSubsystem, DriveConstants.kAutoDriveForwardDistance));
   }
 }
