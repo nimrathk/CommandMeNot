17af4c203505838c23531723759a6bad342211aa
Nimrath K <109793342+nimrathk@users.noreply.github.com>
2024-01-15 19:36:41 -0500

    Create ArcadeDriveCmd.java

---
+++ Commands/ArcadeDriveCmd.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import java.util.function.Supplier;
+
+/** An example command that uses an example subsystem. */
+public class ArcadeDriveCmd extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final DriveSubsystem driveSubsystem;
+  private final Supplier<Double> leftSpeedFunction, rightSpeedFunction;
+
+  /**
+   * Creates a new ArcadeDriveCmd.
+   *
+   * @param driveSubsystem the driving subsystem.
+   *
+   */
+  public ArcadeDriveCmd(DriveSubsystem driveSubsystem, Supplier<Double> leftSpeedFunction, Supplier<Double> rightSpeedFunction) {
+    this.driveSubsystem = driveSubsystem;
+    this.leftSpeedFunction = leftSpeedFunction;
+    this.rightSpeedFunction = rightSpeedFunction;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(driveSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    System.out.println("ArcadeDriveCmd started!");
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    driveSubsystem.setMotors(leftSpeedFunction.get(), rightSpeedFunction.get());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    System.out.println("ArcadeDriveCmd ended!");
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
